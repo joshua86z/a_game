0324197dc77814e9b780ed4ec74aba0f3dba7635
Joshua <fhbzyc@gmail.com>
2014-07-18 00:43:57 +0800

    Update init.go

--- src/controllers/init.go
+++ src/controllers/init.go
@@ -1,60 +1,69 @@
 package controllers
 
 import (
-	"libs/log"
+	"code.google.com/p/go.net/websocket"
+	"fmt"
+	"libs/ssdb"
+	"libs/token"
 	"models"
 	_ "models"
 	"protodata"
-	"reflect"
 	"runtime"
+	"sync"
+	"time"
 )
 
-// 游戏主逻辑
+// 运行变量
 var (
-	login   *Login
-	role    *Role
-	general *General
-	item    *Item
-	pay     *Pay
-	mail    *Mail
+	online          int
+	gameToken       *token.Token
+	playLock        sync.Mutex
+	playerMap       map[int64]*Connect
+	request_log_map map[int32]string
 )
 
 func init() {
-
-	handlers = getHandlerMap()
-	handlerNames = make(map[int32]string)
-	for index, handler := range handlers {
-		handlerNames[index] = runtime.FuncForPC(reflect.ValueOf(handler).Pointer()).Name()
-	}
-
-	log.Info("Program Run !")
+	gameToken = token.NewToken(tokenAdapter{})
+	playerMap = make(map[int64]*Connect)
+	request_log_map = make(map[int32]string)
+	CountOnline()
 }
 
-// handlers
-var (
-	handlers     map[int32]func(*models.RoleModel, *protodata.CommandRequest) (protodata.StatusCode, interface{}, error)
-	handlerNames map[int32]string
-)
+func Handler(ws *websocket.Connect) {
 
-// get command handlers for set handlers
-func getHandlerMap() map[int32]func(*models.RoleModel, *protodata.CommandRequest) (protodata.StatusCode, interface{}, error) {
+	online++
 
-	return map[int32]func(*models.RoleModel, *protodata.CommandRequest) (protodata.StatusCode, interface{}, error){
-		10103: login.Login,
-		10104: role.UserDataRequest,
-		10105: role.SetRoleName,
-		10106: role.RandomName,
-		10107: role.BuyStaminaRequest,
-		10108: pay.BuyCoinRequest,
-		//10109			//补充钻石
-		10110: general.LevelUp,
-		10111: general.Buy,
-		10112: mail.List,
-		10113: mail.MailRewardRequest,
-		10114: item.LevelUp,
-		//10115			//战斗初始化
-		//10116			//战斗结束
+	Connect := &Connect{Conn: ws, Chan: make(chan string, 10)}
+	Connect.pushToClient()
+	Connect.PullFromClient()
+	Connect.Close()
+
+	online--
+}
+
+func SendMessage(uid int64, s string) error {
+	playLock.Lock()
+	if Connect, ok := playerMap[uid]; !ok {
+		return fmt.Errorf("uid : %d not online", uid)
+	} else {
+		Connect.Chan <- s
 	}
+	playLock.Unlock()
+	return nil
+}
+
+func CountOnline() {
+	go func() {
+		//	t := time.Tick(time.Second * 5)
+		t := time.Tick(time.Minute * 5)
+		for {
+			select {
+			case <-t:
+				//	fmt.Println("online num : ", online)
+				models.DB().Exec("INSERT INTO `stat_online`(`online_num`,`online_time`) VALUES (? , NOW())", online)
+			}
+		}
+	}()
 }
 
 func lineNum() protodata.StatusCode {
@@ -63,4 +72,19 @@ func lineNum() protodata.StatusCode {
 		return protodata.StatusCode(line)
 	}
 	return -1
+}
+
+type tokenAdapter struct {
+}
+
+func (this tokenAdapter) Set(key string, value string) error {
+	return ssdb.SSDB().Set(fmt.Sprintf("ALLHERO_%s", key), value)
+}
+
+func (this tokenAdapter) Get(key string) (string, error) {
+	return ssdb.SSDB().Get(fmt.Sprintf("ALLHERO_%s", key))
+}
+
+func (this tokenAdapter) Delete(key string) error {
+	return ssdb.SSDB().Del(fmt.Sprintf("ALLHERO_%s", key))
 }
