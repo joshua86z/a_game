835cee5866ee5f4f3be199a12cf5691d511bbf4d
Joshua <fhbzyc@gmail.com>
2021-08-26 10:10:14 +0800

    Delete src directory

--- src/main/conf/app.lua
+++
@@ -1,7 +0,0 @@
-port = 6940
-level = "Debug"
-
--- 游戏服数据库
-dsn = "root:@tcp(127.0.0.1:3306)/allhero?charset=utf8"
-
-httpPort = 20891
--- src/main/lua/plat_10.lua
+++
@@ -1,5 +0,0 @@
-
--- is ktplay login
-function isLogin(uid, session, sign, other)
-	return uid, true
-end
